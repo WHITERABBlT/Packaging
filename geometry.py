4952300e234b724339b5ae17d531d859ecf14bd2
WHITERABBlT <aantusch@gmail.com>
2025-07-24 16:56:45 +0200

    update

--- geometry.py
+++ geometry.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def area_of_rectangle(length, breadth):
     return length * breadth
 
@@ -8,12 +7,5 @@ def area_of_circle(number):
     """
     Calculates area of a circle.
     """
-=======
-def area_of_rectangle(length, breadth):
-    return length * breadth
-
-import math
-from math import pi
-def area_of_circle(number):
->>>>>>> 1df4cbf2a8a938e74033871754b60f93924ebbeb
+    
     return pi * number ** 2
