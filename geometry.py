83263f8f0dc051ddbceffb211d0337309aff7ccc
WHITERABBlT <aantusch@gmail.com>
2025-07-24 17:17:39 +0200

    added docstrings

--- geometry.py
+++ geometry.py
@@ -1,11 +1,18 @@
+"""
+Geometry file to calculate rectangle and circle areas.
+"""
+
+from math import pi
+
 def area_of_rectangle(length, breadth):
+    """
+    Calculates area of a rectangle.
+    """
     return length * breadth
 
-import math
-from math import pi
+
 def area_of_circle(number):
     """
     Calculates area of a circle.
     """
-    
     return pi * number ** 2
